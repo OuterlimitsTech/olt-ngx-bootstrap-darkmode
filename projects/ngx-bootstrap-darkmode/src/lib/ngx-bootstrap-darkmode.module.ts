af7c619753bf1a3a9b01772bed5177855ee9234e
Chris Straw <chris.straw@outerlimitstech.com>
2022-08-30 20:03:39 -0400

    project create

---
+++ projects/ngx-bootstrap-darkmode/src/lib/ngx-bootstrap-darkmode.module.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [
+
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+  ],
+  exports: [
+
+  ]
+})
+export class NgxBootstrapDarkmodeModule { }
---
+++ projects/ngx-bootstrap-darkmode/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of ngx-bootstrap-darkmode
+ */
+
+export * from './lib/ngx-bootstrap-darkmode.module';
